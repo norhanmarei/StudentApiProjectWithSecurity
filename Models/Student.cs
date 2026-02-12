b7488dc268b1b0f740a602727d5ca28a510c9bd1
Norhan Marei <mustafanour751@gmail.com>
2026-02-12 07:39:15 +0300

    Refactored data model to prepare for JWT-based authentication.

--- Models/Student.cs
+++ Models/Student.cs
@@ -5,6 +5,10 @@ namespace StudentApi.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
-        public int Grade{ get; set; }
+        public int Grade { get; set; }
+        // authentication-related fields
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role{ get; set; }
     }
 }
