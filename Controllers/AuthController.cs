ecb143ddf5fa4899c74b31f37c488be430fbd501
Norhan Marei <mustafanour751@gmail.com>
2026-02-15 10:32:54 +0300

    Add '_GenerateRefreshToken' Method, change 'Login' so it returns both an access token and a refresh token, add 'Refresh' method, and add a 'Logout' method

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using StudentApi.Models;
+using StudentApi.DTOs.Auth;
 using StudentApi.DataSimulation;
 using System.Linq;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
+using System.Security.Cryptography;
 
 
 namespace StudentApi.Controllers
@@ -14,6 +16,13 @@ namespace StudentApi.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
+        private static string _GenerateRefreshToken()
+        {
+            var bytes = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(bytes);
+            return Convert.ToBase64String(bytes);
+        }
         [HttpPost("login")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -40,9 +49,9 @@ namespace StudentApi.Controllers
             var keyString = Environment.GetEnvironmentVariable("STUDENTAPI_DEV_JWT_KEY");
 
             if (string.IsNullOrEmpty(keyString))
-                {
-                  throw new Exception("JWT secret key not found in environment variables.");
-                }   
+            {
+                throw new Exception("JWT secret key not found in environment variables.");
+            }
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyString));
 
@@ -55,11 +64,80 @@ namespace StudentApi.Controllers
                 expires: DateTime.Now.AddMinutes(30),
                 signingCredentials: creds
             );
+            var accessToken = new JwtSecurityTokenHandler().WriteToken(token);
+            var refreshToken = _GenerateRefreshToken();
+            student.RefreshTokenHash = BCrypt.Net.BCrypt.HashPassword(refreshToken);
+            student.RefreshTokenExpiresAt = DateTime.UtcNow.AddDays(7);
+            student.RefreshTokenRevokedAt = null;
 
-            return Ok(new
+
+            return Ok(new TokenResponse
             {
-                token = new JwtSecurityTokenHandler().WriteToken(token)
+                AccessToken = accessToken,
+                RefreshToken = refreshToken
             });
+        }
+        [HttpPost("refresh")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult Refresh([FromBody] RefreshRequest request)
+        {
+            var student = StudentDataSimulation.StudentsList.FirstOrDefault(s => s.Email == request.Email);
+            if (student == null) return Unauthorized("Invalid Refresh Request");
+            if (student.RefreshTokenRevokedAt != null) return Unauthorized("Revoked Refresh Token.");
+            if (student.RefreshTokenExpiresAt == null || student.RefreshTokenExpiresAt <= DateTime.UtcNow)
+                return Unauthorized("Expired Refresh Token.");
+            bool isRefreshTokenValid = BCrypt.Net.BCrypt.Verify(request.RefreshToken, student.RefreshTokenHash);
+            if (!isRefreshTokenValid) return Unauthorized("Invalid Refresh Token.");
+
+
+            var claims = new[]
+           {
+                new Claim(ClaimTypes.NameIdentifier, student.Id.ToString()),
+                new Claim(ClaimTypes.Email, student.Email),
+                new Claim(ClaimTypes.Role, student.Role)
+            };
+            var keyString = Environment.GetEnvironmentVariable("STUDENTAPI_DEV_JWT_KEY");
+
+            if (string.IsNullOrEmpty(keyString))
+            {
+                throw new Exception("JWT secret key not found in environment variables.");
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyString));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "StudentApi",
+                audience: "StudentApiUsers",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                // expires: DateTime.Now.AddSeconds(20),
+                signingCredentials: creds
+            );
+            var newAccessToken = new JwtSecurityTokenHandler().WriteToken(token);
+            var newRefreshToken = _GenerateRefreshToken();
+            student.RefreshTokenHash = BCrypt.Net.BCrypt.HashPassword(newRefreshToken);
+            student.RefreshTokenExpiresAt = DateTime.UtcNow.AddDays(7);
+            student.RefreshTokenRevokedAt = null;
+
+
+            return Ok(new TokenResponse
+            {
+                AccessToken = newAccessToken,
+                RefreshToken = newRefreshToken
+            });
+        }
+        [HttpPost("logout")]
+        public IActionResult Logout([FromBody] LogoutRequest request)
+        {
+            var student = StudentDataSimulation.StudentsList.FirstOrDefault(s => s.Email == request.Email);
+            if (student == null) return Ok();
+            bool isRefreshTokenValid = BCrypt.Net.BCrypt.Verify(student.RefreshTokenHash, request.RefreshToken);
+            if (!isRefreshTokenValid) return Ok();
+            student.RefreshTokenRevokedAt = DateTime.UtcNow;
+            return Ok("Logged Out Successfully.");
         }
     }
 }
